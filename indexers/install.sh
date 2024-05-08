2e952ad4b93510fe57f524aead153d3a5dd1830f
Akira <156126180+akiraonstarknet@users.noreply.github.com>
2024-05-08 10:49:30 +0530

    Update install.sh

--- indexers/install.sh
+++ indexers/install.sh
@@ -6,7 +6,11 @@ echo "ldd version"
 ldd --version | head -n1
 
 # Build GLIBC_2.29 from sources
+echo "updating"
+sudo apt-get update
+echo "installing gawk bison"
 sudo apt-get install gawk bison -y
+echo "installing glibc"
 wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.34.tar.gz
 tar -zxvf glibc-2.34.tar.gz && cd glibc-2.34
 mkdir glibc-build && cd glibc-build

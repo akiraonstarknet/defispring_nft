11379499cf07e184ee31bf9b657f6a6e8512b53c
Akira <156126180+akiraonstarknet@users.noreply.github.com>
2024-05-08 10:28:18 +0530

    Update install.sh

--- indexers/install.sh
+++ indexers/install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Solves this error
+# https://stackoverflow.com/questions/72513993/how-to-install-glibc-2-29-or-higher-in-ubuntu-18-04
+wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
+tar -zxvf glibc-2.29.tar.gz
+mkdir glibc-2.29/build
+cd glibc-2.29/build
+../configure --prefix=/opt/glibc
+make 
+make install
+
+# Install apibara
 curl -sL https://install.apibara.com | bash
 apibara --version
 apibara plugins install sink-postgres

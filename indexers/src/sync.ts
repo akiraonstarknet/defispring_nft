91c89ee7ebf9bbd4f07ed6e657dbdfa9dba18d83
akiraonstarknet <zkdice@proton.me>
2024-11-25 10:31:34 +0530

    nov 25th

--- indexers/src/sync.ts
+++ indexers/src/sync.ts
@@ -25,7 +25,7 @@ console.log(`tracking ${filter.events.length} contracts`);
 
 export const config = {
     streamUrl: "https://mainnet.starknet.a5a.ch",
-    startingBlock: 856814, // ! REMEMBER TO UPDATE THIS ON EACH SYNC
+    startingBlock: 874340, // ! REMEMBER TO UPDATE THIS ON EACH SYNC
     network: "starknet",
     finality: "DATA_STATUS_ACCEPTED",
     filter: filter,
@@ -37,7 +37,7 @@ export const config = {
 };
 
 // ! REMEMBER TO UPDATE THIS ON EACH SYNC
-const MAX_BLOCK = 886824;
+const MAX_BLOCK = 927342;
 
 // Event processor function to store in db
 export default function transform({ header, events }: v1alpha2.Block) {
